ced45de33209b94c923229bbfd3752a43cf9e4d3
glaures <guido@laures.de>
2023-10-21 23:53:31 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-const BASE_PATH = process.env.BASE_PATH || '';
+const BASE_PATH = process.env.BASE_PATH || './';
 
 // https://vitejs.dev/config/
 export default defineConfig({
