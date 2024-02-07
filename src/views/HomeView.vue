218949bec193863af746f66d5242540a85a949d3
glaures <guido@laures.de>
2024-02-07 10:40:12 +0100

    korrekte Fehlerbehandlung

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,54 @@
   <main>
     <div class="container">
       <div class="row mt-5">
-        <input class="form-control form-text shadow" :placeholder="$t('searchLong')"/>
+        <input v-model="searchTerm" class="form-control form-text shadow" :placeholder="$t('searchLong')"/>
+      </div>
+      <div class="mt-4 row">
+        <div class="col-6 col-md-3" v-for="user in foundUsers" :key="user.id">
+          <div class="card">
+            <div class="card-header">
+              <AdvancedImage :cld-img="$cld.image(user.profilePicture)" :height="30" :alt="user.familyName"
+                             class="rounded-circle shadow">
+              </AdvancedImage>
+              {{ user.givenName + ' ' + user.familyName }}
+            </div>
+            <div class="card-body">
+              Lorem ipsum ...
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script>
+import {api} from '@/4bw-api'
+import {handleError} from "@/utils/notifications";
+import {AdvancedImage} from "@cloudinary/vue";
+
+export default {
+  components: {
+    AdvancedImage
+  },
+  data() {
+    return {
+      searchTerm: '',
+      foundUsers: []
+    }
+  },
+  watch: {
+    searchTerm(newValue) {
+      if (newValue && newValue.length > 2) {
+        api.get('/search', {
+          params: {
+            term: newValue,
+            type: 'user'
+          }
+        }).then(res => this.foundUsers = res.data.map(sr => sr.entity))
+            .catch(err => handleError(err))
+      }
+    }
+  }
+}
 </script>
